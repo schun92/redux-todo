227312ad07816844984b173e1210909233fe680c
schun92 <sarahsandiferchun@gmail.com>
2018-07-09 14:38:07 -0700

    Added error handling for single item page

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,23 @@
-import 'materialize-css/dist/css/materialize.min.css'; 
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import List from './list';
 import AddItem from './add_item';
 import ViewItem from './view_item';
+import NotFound from './not_found';
 
 
 
 
 const App = () => (
     <div className="container">
-        <Route exact path = "/" component={List}/>
-        <Route path="/add-item" component={AddItem}/>
-        <Route path="/todo/:item_id" component={ViewItem}/>
-        
+        <Switch>
+            <Route exact path="/" component={List} />
+            <Route path="/add-item" component={AddItem} />
+            <Route path="/todo/:item_id" component={ViewItem} />
+            <Route component={NotFound} />
+        </Switch>
+
     </div>
 );
 
